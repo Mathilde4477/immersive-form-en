39e9dec7acd89c41b20707ab11b56dccf68abb21
Mathilde4477 <visites@immersive-normandy.com>
2025-07-26 23:27:57 +0200

    Update interface_formulaire_en.py

--- interface_formulaire_en.py
+++ interface_formulaire_en.py
@@ -1,10 +1,15 @@
-
 import streamlit as st
 from fpdf import FPDF
 import pandas as pd
 import os
+import datetime
 
-st.title("Formulaire Immersive - Version Complète")
+date_demande = st.date_input("📅 Date de la demande", value=datetime.date.today())
+date_visite = st.date_input("📆 Date de la visite")
+
+st.title("Formulaire Immersive - Version Anglaise")
+
+
 
 # Champs d'identité
 reference = st.text_input("Référence")
